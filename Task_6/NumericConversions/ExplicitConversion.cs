326c2a09c06a5ff28221fec80d603b06621982b2
JOKER <sasha.golubovich@tut.by>
2021-09-16 09:24:02 +0300

    Create Task_6

---
+++ Task_6/NumericConversions/ExplicitConversion.cs
@@ -0,0 +1,64 @@
+﻿namespace NumericConversions
+{
+    public static class ExplicitConversion
+    {
+        public static int LongToInt(long longValue)
+        {
+            return (int)longValue;
+        }
+
+        public static int FloatToInt(float floatValue)
+        {
+            int value = (int)floatValue;
+            return value;
+        }
+
+        public static int DoubleToInt(double doubleValue)
+        {
+            int value = (int)doubleValue;
+            return value;
+        }
+
+        public static int DecimalToInt(decimal decimalValue)
+        {
+            int value = (int)decimalValue;
+            return value;
+        }
+
+        public static long FloatToLong(float floatValue)
+        {
+            long value = (long)floatValue;
+            return value;
+        }
+
+        public static long DoubleToLong(double doubleValue)
+        {
+            long value = (long)doubleValue;
+            return value;
+        }
+
+        public static long DecimalToLong(decimal decimalValue)
+        {
+            long value = (long)decimalValue;
+            return value;
+        }
+
+        public static byte ShortToByte(short shortValue)
+        {
+            byte value = (byte)shortValue;
+            return value;
+        }
+
+        public static byte IntToByte(int intValue)
+        {
+            byte value = (byte)intValue;
+            return value;
+        }
+
+        public static short IntToShort(int intValue)
+        {
+            short value = (short)intValue;
+            return value;
+        }
+    }
+}
---
+++ Task_6/NumericConversions/ImplicitConversion.cs
@@ -0,0 +1,58 @@
+﻿namespace NumericConversions
+{
+    public static class ImplicitConversion
+    {
+        public static long IntToLong(int intValue)
+        {
+            return intValue;
+        }
+
+        public static float IntToFloat(int intValue)
+        {
+            float value = intValue;
+            return value;
+        }
+
+        public static double IntToDouble(int intValue)
+        {
+            double value = intValue;
+            return value;
+        }
+
+        public static decimal IntToDecimal(int intValue)
+        {
+            decimal value = intValue;
+            return value;
+        }
+
+        public static float LongToFloat(long longValue)
+        {
+            float value = longValue;
+            return value;
+        }
+
+        public static double LongToDouble(long longValue)
+        {
+            double value = longValue;
+            return value;
+        }
+
+        public static decimal LongToDecimal(long longValue)
+        {
+            decimal value = longValue;
+            return value;
+        }
+
+        public static short ByteToShort(byte byteValue)
+        {
+            short value = byteValue;
+            return value;
+        }
+
+        public static int ByteToInt(byte byteValue)
+        {
+            int value = byteValue;
+            return value;
+        }
+    }
+}
