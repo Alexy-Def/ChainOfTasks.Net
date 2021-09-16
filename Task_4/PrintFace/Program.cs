7e78dce9c2680a03e38af552ff37fd094da9118d
JOKER <sasha.golubovich@tut.by>
2021-09-16 08:08:25 +0300

    Create Task_4 & Task_5

---
+++ Task_4/PrintFace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace PrintFace
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Hello, world!{0}", Environment.NewLine);
+            string userName = Console.ReadLine();
+            SayHelloUser(userName);
+            PrintFace();
+        }
+
+        public static void SayHelloUser(string userName)
+        {
+            Console.WriteLine("Hello, {0}!", userName);
+        }
+
+        public static void PrintFace()
+        {
+            Console.Write($" +\"\"\"\"\"+{Environment.NewLine}" +
+                              $"(| o o |){Environment.NewLine}" +
+                              $" |  ^  |{Environment.NewLine}" +
+                              $" | '-' |{Environment.NewLine}" +
+                              $" +-----+{Environment.NewLine}");
+        }
+    }
+}
