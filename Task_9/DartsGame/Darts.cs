61f00b88d94871789d0a435803ba54761c3ceb95
JOKER <sasha.golubovich@tut.by>
2021-09-18 19:11:14 +0300

    Create Task_9

---
+++ Task_9/DartsGame/Darts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        public static int GetScore(double x, double y)
+        {
+            double yourRadius = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+            if (yourRadius > 10)
+            {
+                return 0;
+            }
+            else if (yourRadius <= 1)
+            {
+                return 10;
+            }
+            else if (yourRadius <= 5)
+            {
+                return 5;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
