9c3dfb38390bcb98956a726f3d5b5d78af8e0a86
JOKER <sasha.golubovich@tut.by>
2021-10-01 11:39:30 +0300

    Create Task_13

---
+++ Task_13/PopulationTask/Population.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PopulationTask
+{
+    public static class Population
+    {
+        public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("invalid argument", nameof(initialPopulation));
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("invalid argument", nameof(visitors));
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("invalid argument", nameof(currentPopulation));
+            }
+
+            if (currentPopulation < initialPopulation)
+            {
+                throw new ArgumentException("invalid argument", nameof(currentPopulation));
+            }
+
+            if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "invalid argument");
+            }
+
+            var doubleInitialPopulation = Convert.ToDouble(initialPopulation);
+            int counter = 0;
+
+            while (doubleInitialPopulation <= currentPopulation)
+            {
+                doubleInitialPopulation += visitors + (doubleInitialPopulation * percent / 100);
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
